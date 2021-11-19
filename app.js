3ed13f2bdb9ebd60927cba4cd9fc8d820d4c39e8
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2021-11-18 19:46:35 -0500

    Chat with sockets running

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const app = express();
 const http = require('http')
 const server = http.createServer(app);
 
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+io.on('connection', (socket) =>{
+    // console.log('Un usuario se ha conectado');
+    // socket.on('chat',(msg)=>{
+    //     console.log(msg);
+    // });
+    socket.on('chat',(msg)=>{
+        io.emit('chat',msg);
+    });
+
+});
+
 app.get('/',(require, response) =>{
     // response.send('<h1>App Chat Socket</h1>');        
     response.sendFile(`${__dirname}/cliente/index.html`);
